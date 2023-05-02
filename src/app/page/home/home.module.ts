e07f0e443cc1a0f27a4150c2c73497cbea30051d
apriginh0 <apriginh0@hotmail.com>
2023-05-02 13:53:09 -0300

    Conhecendo módulos no Angular

---
+++ src/app/page/home/home.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+/* É nessa pasta PAGES que coloca todos páginas do site */
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+/* É aqui que coloca todos componentes em comum, compartilhado entre várias páginas */
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
